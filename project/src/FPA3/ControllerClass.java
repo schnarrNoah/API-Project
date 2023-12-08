2cbd3f1706e96b1138de1bb7a7e5472b2e12c342
schnarrnoah <schnarrnoah@104WKS12.bsz.edu>
2023-12-08 08:52:31 +0100

    MVC & Listeners

--- project/src/FPA3/ControllerClass.java
+++ project/src/FPA3/ControllerClass.java
@@ -2,9 +2,21 @@ package FPA3;
 
 public class ControllerClass
 {
+	Gui g;
+	
 	public ControllerClass()
+	{
+		this.g = new Gui();
+	}
+	
+	public void setListeners()
+	{
+		g.setBtnSend(new AClass());
+	}
+	
+	
+	class AClass() implements ActionListeners
 	{
 		
 	}
-	
 }
--- project/src/FPA3/Gui.java
+++ project/src/FPA3/Gui.java
@@ -5,9 +5,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import FPA3.ControllerClass.AClass;
+
 import javax.swing.JTextArea;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.event.ActionListener;
 import java.awt.Color;
 import javax.swing.JLabel;
 
